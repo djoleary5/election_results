906906ff73ab8a0b2ece7e4f4a3284958e686f4e
Dennis O'Leary <51426293+djoleary5@users.noreply.github.com>
2020-04-14 18:39:55 -0400

    calculate total votes

--- main.py
+++ main.py
@@ -18,3 +18,21 @@ j = 0
 k = 0
 win = 0
 
+# open input file, remove header row and loop through the remaining rows
+with open(csvpath, newline= "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    for row in csvreader:
+        # add each voter ID to list 'voterIDs'; it's length is the total number of votes
+        voterIDs.append(row[0])
+        # add the candidate for each vote to a list
+        candidates.append(row[2])
+
+# remove duplicates from candidate list
+candidates = list( dict.fromkeys(candidates))
+
+# print output in terminal
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(len(voterIDs)))
+print("-------------------------")
